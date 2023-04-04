bdf924c201f28680aa14c7df6b34ce18d8958e7f
uuip <uuip@users.noreply.github.com>
2023-04-04 22:30:44 +0800

    update

--- c2q.py
+++ c2q.py
@@ -3,16 +3,17 @@ from pathlib import Path
 
 import yaml
 
-key_map = {"DOMAIN-KEYWORD": "host-keyword",
-           "DOMAIN": "host",
-           "DOMAIN-SUFFIX": "host-suffix",
-           "IP-CIDR": "ip-cidr",
-           "IP-CIDR6": "ip6-cidr",
-           "GEOIP": "geoip",
-           }
+key_map = {
+    "DOMAIN-KEYWORD": "host-keyword",
+    "DOMAIN": "host",
+    "DOMAIN-SUFFIX": "host-suffix",
+    "IP-CIDR": "ip-cidr",
+    "IP-CIDR6": "ip6-cidr",
+    "GEOIP": "geoip",
+}
 
-pattern = re.compile(f'({"|".join(x for x in key_map)})(?=\s*?,)')
-drop_pattern = re.compile(',\s*no-resolve')
+pattern = re.compile(rf'({"|".join(x for x in key_map)})(?=\s*?,)')
+drop_pattern = re.compile(r",\s*no-resolve")
 
 
 def subrepl(matchobj):
@@ -24,17 +25,17 @@ def wrap_sub(string):
 
 
 def wrap_drop(string):
-    return drop_pattern.sub('', string)
+    return drop_pattern.sub("", string)
 
 
-for f in Path('.').glob('*.yaml'):
-    f_io = f.open(encoding='utf8')
+for f in Path(".").glob("*.yaml"):
+    f_io = f.open(encoding="utf8")
     clash = yaml.safe_load(f_io)
     f_io.close()
     step1 = filter(lambda x: x and not x.startswith("PROCESS-NAME"), clash["payload"])
     step2 = map(wrap_sub, step1)
     step3 = map(wrap_drop, step2)
-    step4 = map(lambda x: f'{x},{f.stem}\n'.lower(), step3)
+    step4 = map(lambda x: f"{x},{f.stem}\n".lower(), step3)
 
-    with open("qx" / f.with_suffix('.list'), 'w+', encoding='utf8', newline='\n') as qx:
+    with open("qx" / f.with_suffix(".list"), "w+", encoding="utf8", newline="\n") as qx:
         qx.writelines(step4)
